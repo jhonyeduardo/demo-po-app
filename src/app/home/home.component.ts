e57acb4c5c29dbdce81716bfa6b7200eed555870
jhonyeduardo <jhonyeduardo96@gmail.com>
2021-06-02 04:53:09 -0300

    docs: funcionalidades para testar migração

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoLineChartSeries, PoChartType, PoPieChartSeries, PoDonutChartSeries, PoBarChartSeries, PoColumnChartSeries } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  // TYPES
+  lineType: PoChartType = PoChartType.Line;
+  columnType: PoChartType = PoChartType.Column;
+  barType: PoChartType = PoChartType.Bar;
+  donutType: PoChartType = PoChartType.Donut;
+  pieType: PoChartType = PoChartType.Pie;
+
+  lineSeries: Array<PoLineChartSeries> = [
+    { label: 'JOE', data: [123, 50] }
+  ];
+
+  columnSeries: Array<PoColumnChartSeries> = [
+    { label: 'JOE', data: [123, 50] }
+  ];
+
+  barSeries: Array<PoBarChartSeries> = [
+    { label: 'JOE', data: [123, 50] }
+  ];
+
+  donutSeries: Array<PoDonutChartSeries> = [
+    { label: 'JOE', data: 50 },
+    { label: 'JANE', data: 20 }
+  ];
+
+  pieSeries: Array<PoPieChartSeries> = [
+    { label: 'JOE', data: 123 },
+    { label: 'JANE', data: 10 }
+  ];
 
   constructor() { }
 
